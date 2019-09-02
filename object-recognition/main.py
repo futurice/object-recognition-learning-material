6dc60fd1f95dce1ad599954b43d679e27d5e1ba8
NestorK <nestor.kohler@futurice.com>
2019-09-02 14:17:33 +0300

    Add dummy starting point with instructions for first step: loading the images

--- object-recognition/main.py
+++ object-recognition/main.py
@@ -1,4 +1,24 @@
 import argparse
+import logging
+
+logger = logging.getLogger()
+
+def find_model_in_target(path_to_model: str, path_to_target: str):
+    
+    # STEP 1
+    # Regardless of your approach, you first need to load the chosen images from
+    # the given files. Most techniques use gray-scale images for detecting
+    # features, so you'll probably want to load the images as gray-scale.
+    # While loading the images, there are a couple of things you might want to consider:
+    # 1. Consider doing some smoothing of the image. Noise can otherwise cause unwanted
+    #    discrepancies when trying to find prominent features. Smoothing with OpenCV:
+    #    https://docs.opencv.org/3.1.0/d4/d13/tutorial_py_filtering.html
+    # 2. Scale the images up or down. It's a good idea to have some consitency in
+    #    the image sizes, so consider scaling the images so e.g. the longer side
+    #    is always some specific length. Scaling (and more):
+    #    https://docs.opencv.org/trunk/da/d6e/tutorial_py_geometric_transformations.html
+
+    return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -16,3 +36,9 @@ if __name__ == '__main__':
         type=str,
         help='Path of the target image in which you are trying to find an object.'
     )
+    args = parser.parse_args()
+
+    try:
+        find_model_in_target(args.model_path, args.target_path)
+    except:
+        logger.exception('Unexpected exception occured!')
